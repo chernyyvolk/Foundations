e52046a248f60c30c1c9065ea594c6438a993fc3
Chernyy Volk <calicojones59@gmail.com>
2024-02-12 23:18:51 -0500

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+// Game Loop Variables and functions
+let ticker = 0;
+let population = 0;
+let age = 0;
+
+let dirt = 0;
+let stone = 0;
+let wood = 0;
+let berries = 0;
+
+setInterval(tick, 1000);
+
+function tick() {
+    document.getElementById("demo").innerHTML = ticker++;
+
+    if (ticker >= 10) {
+        ticker = 0;
+        update();
+    }
+}
+
+function update() {
+
+}
